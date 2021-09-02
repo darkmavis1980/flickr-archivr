4e5ec385c387bf836b58d73ec75b71693a07e9a8
Alessio Michelini <alessio@musicphotographer.eu>
2021-09-02 21:48:06 +0100

    Adding structs for albums Adding logic to read albums json file Added sql for new tables

--- init.sql
+++ init.sql
@@ -6,3 +6,24 @@ CREATE TABLE flickr.photos (
 	flickr_id VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id)
 );
+
+CREATE TABLE flickr.albums (
+	id INT NOT NULL AUTO_INCREMENT,
+	title VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+)
+
+CREATE TABLE flickr.album_photos (
+	id INT NOT NULL AUTO_INCREMENT,
+	photo_id INT NOT NULL,
+	album_id INT NOT NULL,
+	INDEX ph_id (photo_id),
+	INDEX al_id (album_id),
+	PRIMARY KEY (id)
+	-- FOREIGN KEY (photo_id)
+	-- 	REFERENCES flickr.photos(id)
+	-- 	ON DELETE CASCADE,
+	-- FOREIGN KEY (album_id)
+	-- 	REFERENCES flickr.albums(id)
+	-- 	ON DELETE CASCADE
+)
--- main.go
+++ main.go
@@ -2,16 +2,35 @@ package main
 
 import (
     "database/sql"
+    "encoding/json"
     "time"
     "fmt"
     "regexp"
     "io/ioutil"
     "log"
+    // "os"
     _ "github.com/go-sql-driver/mysql"
 )
 
+type Album struct {
+    PhotoCount  string   `json:"photo_count"`
+    ID          int      `json:"id"`
+    URL         string   `json:"url"`
+    Title       string   `json:"title"`
+    Description string   `json:"description"`
+    ViewCount   string   `json:"view_count"`
+    Created     string   `json:"created"`
+    LastUpdated string   `json:"last_updated"`
+    CoverPhoto  string   `json:"cover_photo"`
+    Photos      []string `json:"photos"`
+}
+
+type Albums struct {
+    Albums      []Album  `json:"albums"`
+}
+
 func fetchFiles() []string {
-    files, err := ioutil.ReadDir("./account")
+    files, err := ioutil.ReadDir("./data/account")
     if err != nil {
         log.Fatal(err)
     }
@@ -39,6 +58,45 @@ func findPhotos() []string {
     return photos
 }
 
+func resetTable(db *sql.DB) {
+    truncate, err := db.Query("TRUNCATE TABLE photos")
+    if err != nil {
+        panic(err.Error())
+    }
+
+    truncate.Close()
+}
+
+func insertPhotos(db *sql.DB, photos []string) {
+    var re = regexp.MustCompile(`(?m)photo_([0-9]+).json`)
+
+    for _, filename := range photos {
+        var flickrId = re.FindStringSubmatch(filename)[1]
+        insert, err := db.Query("INSERT INTO photos (filename, flickr_id) VALUES (?, ?) RETURNING id", filename, flickrId)
+        // if there is an error inserting, handle it
+        if err != nil {
+            panic(err.Error())
+        }
+        insert.Close()
+    }
+}
+
+func findAlbums() Albums {
+    file, err := ioutil.ReadFile("data/account/albums.json")
+
+    if err != nil {
+        panic(err.Error())
+    }
+
+    data := Albums{}
+
+    _ = json.Unmarshal([]byte(file), &data)
+
+    fmt.Printf("Found %d \n", len(data.Albums))
+
+    return data
+}
+
 func main() {
     db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/flickr")
     if err != nil {
@@ -49,29 +107,20 @@ func main() {
     db.SetMaxOpenConns(10)
     db.SetMaxIdleConns(10)
 
-    var photos = findPhotos()
+    // var photos = findPhotos()
+    var albums = findAlbums()
 
-    // for _, f := range photos {
-    // 	fmt.Println(f)
-    // }
-    fmt.Printf("Found %d files\n", len(photos))
+    fmt.Println(albums.Albums[0].Title)
+
+    //resetTable(db)
+
+    //fmt.Printf("Found %d files\n", len(photos))
 
     // defer the close till after the main function has finished
     // executing
     defer db.Close()
 
-    var re = regexp.MustCompile(`(?m)photo_([0-9]+).json`)
-
-    for _, filename := range photos {
-        var flickrId = re.FindStringSubmatch(filename)[1]
-        insert, err := db.Query("INSERT INTO photos (filename, flickr_id) VALUES (?, ?)", filename, flickrId)
-
-        // if there is an error inserting, handle it
-        if err != nil {
-            panic(err.Error())
-        }
-        insert.Close()
-    }
+    // insertPhotos(db, photos)
 
     fmt.Println("Finished!")
 }
